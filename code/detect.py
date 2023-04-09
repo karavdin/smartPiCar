94995b66faa1c18d23fc95808e4fb19c91facb8d
ywbb <anastasia.karavdina@jda.com>
2023-04-09 22:38:00 +0200

    minor changes

--- code/detect.py
+++ code/detect.py
@@ -120,13 +120,13 @@ def main():
       help='Width of frame to capture from camera.',
       required=False,
       type=int,
-      default=640)
+      default=1920)
   parser.add_argument(
       '--frameHeight',
       help='Height of frame to capture from camera.',
       required=False,
       type=int,
-      default=480)
+      default=1080)
   parser.add_argument(
       '--numThreads',
       help='Number of CPU threads to run the model.',
